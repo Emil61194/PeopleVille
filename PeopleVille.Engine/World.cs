1522cd0e7841d20b2b709d025e92a1df40199764
Thomas Bo Rasmussen <61274@edu.sde.dk>
2026-09-16 11:21:21 +0200

    Implemented game engine and world

---
+++ PeopleVille.Engine/World.cs
@@ -0,0 +1,45 @@
+﻿using PeopleVille.Core.Models;
+using PeopleVille.Core.Models.Home;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleVille.Engine
+{
+    public class World
+    {
+        private int time;
+        private int weekDay;
+
+        public List<Citizen>? Citizens { get; set; }
+        public List<BankAccount>? BankAccount { get; set; }
+        public List<Job>? Jobs { get; set; }
+        public List<ShoppingCenter>? ShoppingCenters { get; set; }
+        public List<House>? Houses { get; set; }
+        public List<Apartment>? Apartments { get; set; }
+        public List<School>? Schools { get; set; }
+
+        public int Time
+        {
+            get => time;
+            set
+            {
+                if (value >= 24)
+                    time = 1;
+                else
+                    time = value;
+            }
+        }
+        public int WeekDay
+        {
+            get => weekDay;
+            set
+            {
+                if (value >= 7)
+                    weekDay = 1;
+                else
+                    weekDay = value;
+            }
+        }
+    }
+}
