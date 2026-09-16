8b3a838ec98f91a3f942e0a514e66c7937e8ba01
Thomas Bo Rasmussen <61274@edu.sde.dk>
2026-09-16 12:06:34 +0200

    Citizen loop

--- PeopleVille.Core/Models/Citizen.cs
+++ PeopleVille.Core/Models/Citizen.cs
@@ -1,6 +1,7 @@
 ﻿using PeopleVille.Core.Models.Home;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Text;
 
 namespace PeopleVille.Core.Models
@@ -13,10 +14,31 @@ namespace PeopleVille.Core.Models
         public DateTime Birth { get; } = birth;
         public int Gender { get; } = gender;
         public Job? Job { get; set; }
-        public Building? CurrentLocation { get; set; }
+        public string HomeAddress { get; set; }
+
+        public required string CurrentLocation { get; set; }
 
         public void DoSomething()
         {
+            TimeSpan age = DateTime.Now - Birth;
+            int yearsOld = (int)(age.TotalDays / 365.25);
+    
+            if (world.Time > 21 || world.Time < 6)
+            {
+                CurrentLocation = HomeAddress;
+            }
+            else if (yearsOld > 18 && Job != null && Job.WorkStartTime < world.Time && Job.WorkEndTime > world.Time)
+            {
+                CurrentLocation = Job.Workplace.Address;
+            }
+            else if (yearsOld > 18 && Job == null)
+            {
+                CurrentLocation = HomeAddress; // Maybe later on activites for jobless people
+            }
+            else if (yearsOld < 18)
+            {
+                CurrentLocation = HomeAddress; // Add school later on
+            }
 
         }
     }
--- PeopleVille.Core/Models/Home/ShoppingCenter.cs
+++ PeopleVille.Core/Models/Home/ShoppingCenter.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using PeopleVille.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PeopleVille.Core.Models.Home
 {
-    public class ShoppingCenter : Building
+    public class ShoppingCenter : Building, IWorkplace
     {
         public override required string Address { get; set; }
         public override int FoodInventory { get; set; }
