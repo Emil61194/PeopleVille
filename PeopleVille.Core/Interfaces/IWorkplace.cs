60afa9e4354aebcf5a7ac5bcd3319574304542fd
Thomas Bo Rasmussen <148067510+thom227v@users.noreply.github.com>
2026-09-16 12:09:38 +0200

    Feature/game enginge implementation (#10)
    * Implemented game engine and world
    
    * Citizen loop

---
+++ PeopleVille.Core/Interfaces/IWorkplace.cs
@@ -0,0 +1,14 @@
+﻿using PeopleVille.Core.Models;
+using PeopleVille.Core.Models.Home;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleVille.Core.Interfaces
+{
+    public interface IWorkplace
+    {
+        public string Address { get; set; }
+
+    }
+}
--- PeopleVille.Core/Models/Job.cs
+++ PeopleVille.Core/Models/Job.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PeopleVille.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata;
 using System.Text;
@@ -9,7 +10,8 @@ namespace PeopleVille.Core.Models
     {
         public required string Title { get; set; }
         public decimal Salary { get; set; }
-        public DateTime WorkStartTime { get; set; }
-        public DateTime WorkEndTime { get; set; }
+        public int WorkStartTime { get; set; }
+        public int WorkEndTime { get; set; }
+        public required IWorkplace Workplace { get; set; }
     }
 }
