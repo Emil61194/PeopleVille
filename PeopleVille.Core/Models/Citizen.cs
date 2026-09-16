b61d96993cefc815fd3c933a0f1f0096b5a83fb1
1kb_dev <160396234+1kb-dev@users.noreply.github.com>
2026-09-16 13:09:24 +0200

    Implemented Game Loop (Day/Night) (#15)
    * Feature/game enginge implementation (#10)
    
    * Implemented game engine and world
    
    * Citizen loop
    
    * Implemented IsNight (#14)
    
    * We don't want to create dateTime functionality that already exists in C#. Therefor ive changed 2 variable int 1 dateTime variable.
    
    ---------
    
    Co-authored-by: Thomas Bo Rasmussen <148067510+thom227v@users.noreply.github.com>
    Co-authored-by: emilh <61194@edu.sde.dk>

--- PeopleVille.Core/Models/Citizen.cs
+++ PeopleVille.Core/Models/Citizen.cs
@@ -1,17 +1,45 @@
 ﻿using PeopleVille.Core.Models.Home;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Text;
 
 namespace PeopleVille.Core.Models
 {
-    public class Citizen
+    public class Citizen(World world, int id, string firstName, string lastName, DateTime birth, int gender)
     {
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public DateTime Birth { get; set; }
-        public int Gender { get; set; }
+        public int Id { get; set; } = id;
+        public string FirstName { get; } = firstName;
+        public string LastName { get; } = lastName;
+        public DateTime Birth { get; } = birth;
+        public int Gender { get; } = gender;
         public Job? Job { get; set; }
-        public Building? CurrentLocation { get; set; }
+        public string HomeAddress { get; set; }
+
+        public required string CurrentLocation { get; set; }
+
+        public void DoSomething()
+        {
+            TimeSpan age = DateTime.Now - Birth;
+            int yearsOld = (int)(age.TotalDays / 365.25);
+    
+            if (world.Time > 21 || world.Time < 6)
+            {
+                CurrentLocation = HomeAddress;
+            }
+            else if (yearsOld > 18 && Job != null && Job.WorkStartTime < world.Time && Job.WorkEndTime > world.Time)
+            {
+                CurrentLocation = Job.Workplace.Address;
+            }
+            else if (yearsOld > 18 && Job == null)
+            {
+                CurrentLocation = HomeAddress; // Maybe later on activites for jobless people
+            }
+            else if (yearsOld < 18)
+            {
+                CurrentLocation = HomeAddress; // Add school later on
+            }
+
+        }
     }
 }
---
+++ PeopleVille.Engine/GameEngine.cs
@@ -0,0 +1,78 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using PeopleVille.Core.Models;
+
+namespace PeopleVille.Engine
+{
+    public class GameEngine
+    {
+        private World? world;
+        public bool doPause = false;
+        public event Action? Tick;
+
+        public void Initialize(string filePath = "")
+        {
+            world = CheckSave(filePath);
+            foreach (Citizen citizen in world.Citizens)
+            {
+                Tick += citizen.DoSomething;
+            }
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                // Tid, (wait e.g 1 second) CHECK
+                // Citizens do something ( With Events - Delegete)
+                Tick?.Invoke();
+
+
+
+                // Random stuff happening ( e.g heatstroke, lack of supplies in town = death, virus ) 
+
+                // When Ciitzens home at eating house, reduce private home inventory
+
+                // Wait 1 second
+                world.currentDateTime.AddHours(1);
+                Thread.Sleep(1000);
+                // if pause, 
+                while (doPause)
+                {
+                    // Check for user input to resume or exit
+                    //
+                }
+            }
+        }
+
+        public World CheckSave(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                World? save = JsonSerializer.Deserialize<World>(File.ReadAllText(filePath));
+                if (save == null)
+                {
+                    throw new Exception("Save file is empty or corrupted.");
+                }
+                return save;
+            }
+            else
+            {
+                throw new NotImplementedException();
+                return InitializeCity();
+            }
+        }
+
+        public World InitializeCity()
+        {
+            World save = new World();
+            save.Citizens = new List<Core.Models.Citizen>();
+            save.BankAccount = new List<Core.Models.BankAccount>();
+            save.Jobs = new List<Core.Models.Job>();
+            save.ShoppingCenters = new List<Core.Models.Home.ShoppingCenter>();
+            save.Houses = new List<Core.Models.Home.House>();
+            save.Apartments = new List<Core.Models.Home.Apartment>();
+            save.Schools = new List<Core.Models.Home.School>();
+            return save;
+        }
+    }
+}
