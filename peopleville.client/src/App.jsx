56a6bc928956fdbf3eed3871fb8720e73f7a292c
Th1xDev <142332516+wildREA@users.noreply.github.com>
2026-09-17 09:12:41 +0200

    add: basic world ui (#21)
    Co-authored-by: Th1xDev <29796183-wildREA@users.noreply.gitlab.com>

--- peopleville.client/src/App.jsx
+++ peopleville.client/src/App.jsx
@@ -8,33 +8,37 @@ function App() {
         populateWeatherData();
     }, []);
 
+    function getMapItem(itemName) {
+        return <div><p>{itemName}</p></div>;
+    }
+
+    const mapItems = [
+        "hC",
+        "wC",
+        "mC"
+    ];
+
     const contents = forecasts === undefined
         ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
+        : <div id="mapContainer">
+            {mapItems.map((name) => (
+                <div id="menuDiv" name={name} key={name}>
+                    {getMapItem(name)}
+                    {getMapItem(name)}
+                    {getMapItem(name)}
+                    {getMapItem(name)}
+                    {getMapItem(name)}
+                    {getMapItem(name)}
+                    {getMapItem(name)}
+                    {getMapItem(name)}
+                </div>
+            ))}
+        </div>;
 
     return (
         <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
+            <h1 id="tableLabel">PeopleVille</h1>
+            <p id="tableDescription">Map</p>
             {contents}
         </div>
     );
