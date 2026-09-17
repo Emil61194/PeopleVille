9c9d83103c5140778bbfcdaf283316fbc4294e30
Emil61194 <61194@edu.sde.dk>
2026-09-17 11:15:02 +0200

    Duplicate variables? (#30)
    Not suitable to override?

--- PeopleVille.Core/Models/World.cs
+++ PeopleVille.Core/Models/World.cs
@@ -19,39 +19,5 @@ namespace PeopleVille.Core.Models
         public List<House> Houses { get; set; } = new List<House>();
         public List<Apartment> Apartments { get; set; } = new List<Apartment>();
         public List<School> Schools { get; set; } = new List<School>();
-
-        public List<Citizen>? Citizens { get; set; }
-        public List<BankAccount>? BankAccount { get; set; }
-        public List<Job>? Jobs { get; set; }
-        public List<ShoppingCenter>? ShoppingCenters { get; set; }
-        public List<House>? Houses { get; set; }
-        public List<Apartment>? Apartments { get; set; }
-        public List<School>? Schools { get; set; }
-        public bool IsNight { get; set; }
-
-        public int Time
-        {
-            get => time;
-            set
-            {
-                if (value >= 24)
-                    time = 1;
-                else
-                    time = value;
-                if (time < 6 && time > 21) IsNight = true;
-                else IsNight = false;
-            }
-        }
-        public int WeekDay
-        {
-            get => weekDay;
-            set
-            {
-                if (value >= 7)
-                    weekDay = 1;
-                else
-                    weekDay = value;
-            }
-        }
     }
 }
