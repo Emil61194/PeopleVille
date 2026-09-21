a9dbf226395b84aa44b1e9de91b4d78047a323d7
1kb_dev <160396234+1kb-dev@users.noreply.github.com>
2026-09-21 10:13:54 +0200

    Feature/frontend prompt for save file [TESTED] (#40)
    * frontend load save files
    
    * Added Cors and Patch for endpoint call
    
    * backend save file api
    
    ---------
    
    Co-authored-by: Emil61194 <61194@edu.sde.dk>

--- peopleville.client/src/App.jsx
+++ peopleville.client/src/App.jsx
@@ -1,55 +1,68 @@
-﻿import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { MainGameButtons } from "./components/MainGameButtons.jsx";
 
 function App() {
-    const [forecasts, setForecasts] = useState();
+  const [forecasts, setForecasts] = useState();
 
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
+  useEffect(() => {
+    populateWeatherData();
+  }, []);
 
-    function getMapItem(itemName) {
-        return <div><p>{itemName}</p></div>;
-    }
-
-    const mapItems = [
-        "hC",
-        "wC",
-        "mC"
-    ];
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <div id="mapContainer">
-            {mapItems.map((name) => (
-                <div id="menuDiv" name={name} key={name}>
-                    {getMapItem(name)}
-                    {getMapItem(name)}
-                    {getMapItem(name)}
-                    {getMapItem(name)}
-                    {getMapItem(name)}
-                    {getMapItem(name)}
-                    {getMapItem(name)}
-                    {getMapItem(name)}
-                </div>
-            ))}
-        </div>;
-
+  function getMapItem(itemName) {
     return (
-        <div>
-            <h1 id="tableLabel">PeopleVille</h1>
-            <p id="tableDescription">Map</p>
-            {contents}
-        </div>
+      <div>
+        <p>{itemName}</p>
+      </div>
     );
-    
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        if (response.ok) {
-            const data = await response.json();
-            setForecasts(data);
-        }
+  }
+
+  const mapItems = ["hC", "wC", "mC"];
+
+  const contents =
+    forecasts === undefined ? (
+      <p>
+        <em>
+          Loading... Please refresh once the ASP.NET backend has started. See{" "}
+          <a href="https://aka.ms/jspsintegrationreact">
+            https://aka.ms/jspsintegrationreact
+          </a>{" "}
+          for more details.
+        </em>
+      </p>
+    ) : (
+      <div id="mapContainer">
+        {mapItems.map((name) => (
+          <div id="menuDiv" name={name} key={name}>
+            {getMapItem(name)}
+            {getMapItem(name)}
+            {getMapItem(name)}
+            {getMapItem(name)}
+            {getMapItem(name)}
+            {getMapItem(name)}
+            {getMapItem(name)}
+            {getMapItem(name)}
+          </div>
+        ))}
+      </div>
+    );
+
+  return (
+    <div>
+      <h1 id="tableLabel">PeopleVille</h1>
+      <p id="tableDescription">Map</p>
+      {contents}
+      <MainGameButtons />
+    </div>
+  );
+
+  async function populateWeatherData() {
+    const response = await fetch("weatherforecast");
+    if (response.ok) {
+      const data = await response.json();
+      setForecasts(data);
     }
+  }
 }
 
 export default App;
