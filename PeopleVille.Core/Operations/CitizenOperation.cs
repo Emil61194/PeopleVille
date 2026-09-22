28b23f8e34d77d417ff7d0a030478130129901cb
Thomas Bo Rasmussen <148067510+thom227v@users.noreply.github.com>
2026-09-22 09:11:48 +0200

    Now sending logs from backend to frontend (#68)

---
+++ PeopleVille.Core/Operations/CitizenOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleVille.Core.Operations
+{
+    public class CitizenOperation
+    {
+        public int CitizenId { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Gender { get; set; }
+        public required string HomeAddress { get; set; }
+        public required string CurrentLocation { get; set; }
+        public bool IsAdult { get; set; }
+        public bool IsEmployed { get; set; }
+        public required string Message { get; set; }
+    }
+}
