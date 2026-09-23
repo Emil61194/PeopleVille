2b276754c756fb86bb50a9551446f0c144ad7b53
Thomas Bo Rasmussen <148067510+thom227v@users.noreply.github.com>
2026-09-23 09:17:01 +0200

    Feature/implement logging from backend (#78)
    * Now sending logs from backend to frontend
    
    * mi gusta
    
    * a
    
    * a

--- PeopleVille.Engine/GameEngine.cs
+++ PeopleVille.Engine/GameEngine.cs
@@ -15,8 +15,7 @@ namespace PeopleVille.Engine
         public World? CurrentWorld => _world;
         public bool Ready = false;
         public bool _doPause = false;
-        public event Action? Tick;
-        public required ConcurrentBag<object> ActionsEachTickChanged;
+        public event Action Tick;
 
         public ConcurrentBag<object> actionsEachTick = new ConcurrentBag<object>();
         public bool Initialize(string? filePath = null)
@@ -48,8 +47,6 @@ namespace PeopleVille.Engine
             while (GameRunning)
             {
                 Tick?.Invoke();
-                await _eventPublisher.PublishEvent("Yo");
-
 
 
                 // Random stuff happening ( e.g heatstroke, lack of supplies in town = death, virus )
@@ -110,7 +107,7 @@ namespace PeopleVille.Engine
             apartmentBuilder.BuildApartments(save);
 
             CitizenBuilder citizenBuilder = new CitizenBuilder();
-            citizenBuilder.BuildCitizens(save, Tick, ActionsEachTickChanged);
+            citizenBuilder.BuildCitizens(save, ref Tick, actionsEachTick);
 
             save.BankAccount = new List<BankAccount>();
 
