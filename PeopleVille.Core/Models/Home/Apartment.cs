2543f00fc9577a414c1ce712e74023ef054668c2
Th1xDev <29796183-wildREA@users.noreply.gitlab.com>
2026-09-24 11:15:38 +0200

    remove: bank account ownership from private homes

--- PeopleVille.Core/Models/Home/Apartment.cs
+++ PeopleVille.Core/Models/Home/Apartment.cs
@@ -4,13 +4,13 @@ namespace PeopleVille.Core.Models.Home
 {
     public class Apartment : Building, IPrivateHome
     {
+        public int HomeId { get; set; }
         public decimal Rent { get; set; }
         public int Floors { get; set; }
         public int CitizenCapacity { get; set; }
         public override required string Address { get; set; }
         public int FoodInventory { get; set; }
         public int WaterInventory { get; set; }
-        public BankAccount BankAccount { get; set; } = new BankAccount();
-
+        public BankAccount HouseholdFunds { get; set; } = new BankAccount();
     }
 }
--- PeopleVille.Core/Models/Home/House.cs
+++ PeopleVille.Core/Models/Home/House.cs
@@ -4,11 +4,11 @@ namespace PeopleVille.Core.Models.Home
 {
     public class House : Building, IPrivateHome
     {
+        public int HomeId { get; set; }
         public int CitizenCapacity { get; set; }
         public override required string Address { get; set; }
         public int FoodInventory { get; set; }
         public int WaterInventory { get; set; }
-        public BankAccount BankAccount { get; set; } = new BankAccount();
-
+        public BankAccount HouseholdFunds { get; set; } = new BankAccount();
     }
 }
